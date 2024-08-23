77c3a1de73a9af320ae600e0cbc95ed54e296ff9
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-08-23 16:25:54 +0800

    routers

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const router1 = express.Router();
+const router2 = express.Router();
+
+router1.get("/user", (request, response, next) => {
+    response.status(200).send("User Router Working");
+});
+router2.get("/admin", (request, response, next) => {
+    response.status(200).send("Admin Router Working");
+});
+
+app.use(router1);
+app.use(router2);
+
 app.get("/", (request, response) => {
     response.status(200).send("Hello World");
 });
